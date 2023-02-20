7eedec603541b2ad33488f0e8e74d372e75ccd5e
tuanLee1257 <tuanle32411@gmail.com>
2023-02-20 17:04:53 +0700

    fix requestMatchers

--- src/main/java/com/example/mysecurity/mysecurity/config/SecurityConfig.java
+++ src/main/java/com/example/mysecurity/mysecurity/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/auth")
+                .requestMatchers("/auth/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
